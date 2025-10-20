ee2bc4190f7f1c5e537de801cb2ffc8c71032f8d
halimbagusalhakim <halsat132@gmail.com>
2025-10-20 09:46:52 +0700

    buat connection ke database

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Create a connection to the database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Elang102',
+    database: 'mahasiswa',
+    port: 3308
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err.stack);
+        return;
+    }
+    console.log('Connected to the MySQL database.');
+});
